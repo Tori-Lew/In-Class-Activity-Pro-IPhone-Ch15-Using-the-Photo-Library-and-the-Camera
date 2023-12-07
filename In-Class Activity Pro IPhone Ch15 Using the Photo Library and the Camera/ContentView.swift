38bc8ecfa8deed9e1201008c57214eee61d3f130
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-12-06 16:06:59 -0800

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch15 Using the Photo Library and the Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch15 Using the Photo Library and the Camera
+//
+//  Created by Student Account on 12/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch15 Using the Photo Library and the Camera/In_Class_Activity_Pro_IPhone_Ch15_Using_the_Photo_Library_and_the_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch15_Using_the_Photo_Library_and_the_CameraApp.swift
+//  In-Class Activity Pro IPhone Ch15 Using the Photo Library and the Camera
+//
+//  Created by Student Account on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch15_Using_the_Photo_Library_and_the_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
